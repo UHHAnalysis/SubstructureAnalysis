88d028feca74b6d03aed9e8bf1538b85cea1a10c
tordreyer <torben.dreyer@desy.de>
2016-05-02 20:33:50 +0200

    fixed includes

--- src/SubstructureCycle.cxx
+++ src/SubstructureCycle.cxx
@@ -3,7 +3,7 @@
 #include <iostream>
 
 using namespace std;
-
+#include "include/SubstructureCycle.h"
 
 #include "SFrameAnalysis/include/SelectionModules.h"
 #include "SFrameAnalysis/include/HypothesisHists.h"
